cf53bcb8b10f84ad1b5c4bdc4fba4e7ec14fa957
MHovenkamp <maaike.hovenkamp@student.hu.nl>
2020-05-11 12:20:18 +0200

    preprocessing in de fer_client

--- src/fer_client.py
+++ src/fer_client.py
@@ -4,7 +4,7 @@ Client that talks to the fer_server.py
 This is usually run on the Raspberry Pi, whereas the fer_server should be run on the server/desktop.
 """
 
-import requests
+import requestsimport cv2
 
 
 class FERClient:
@@ -21,7 +21,30 @@ class FERClient:
             Dictionary with keys 'emotions', 'probabilities' and 'runtime'. Runtime has a dict with 'model', 'grayscale', and 'resize'. Runtime is given in milliseconds.
             Emotions/probabilities are ordered and sorted by descending confidence.
         """
-        files = {"content": open(file, "rb")}
+        # preprocessing
+        #importeren van het model: https://github.com/opencv/opencv/tree/master/data/haarcascades
+        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        
+        image = cv2.imread( file )
+        
+        imageRGB = cv2.cvtColor( file, cv2.COLOR_BGR2RGB)
+        imageCopy = imageRGB.copy()
+        imageGray = cv2.cvtColor( imageRGB, cv2.COLOR_RGB2GRAY )
+
+        face = faceCascade.detectMultiScale( imageGray, 1.25, 6)
+        if not face:
+            return
+        else:
+            #gezichten gevonden dus eerst croppen en dan resizen
+            for (x, y, w, h) in face:
+                cv2.rectangle(imageCopy, (x,y), (x+w, y+h), (255,0,0), 3)
+                #croppen van gezicht
+                faceCrop = imageGray[y:y+h, x:x+w]
+                #resizen
+                dim = (64, 64)
+                resized = cv2.resize(faceCrop, dim, interpolation = methods[i])
+                finishedImage = cv2.imencode('.png', resized)
+        files = {"content": finishedImage.tostring()}
         r = requests.post(self.host + "/predict", files=files)
         return r.json()
 
